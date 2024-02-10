50f815bb6e63046c2258d2c69619e353217f80ba
Ahsan Waseem <wahsan77@yahoo.com>
2024-02-09 18:34:47 -0800

    google analytics new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,27 +5,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* Google Tag Manager */}
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-YX7YC49P3J"
+          ></script>
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-M6W3RVCH');`,
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-YX7YC49P3J');
+              `,
             }}
           />
         </Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-M6W3RVCH"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
           <Main />
           <NextScript />
         </body>
